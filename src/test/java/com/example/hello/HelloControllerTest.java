e4c390939edc2d029cf1160b0bc497fcc5991c2a
safaa <128332698+Safaa1033@users.noreply.github.com>
2026-01-11 15:06:34 +0100

    Test webhook jenkins v6

--- src/test/java/com/example/hello/HelloControllerTest.java
+++ src/test/java/com/example/hello/HelloControllerTest.java
@@ -20,6 +20,6 @@ class HelloControllerTest {
     void helloEndpointReturnsMessage() throws Exception {
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Bonsoir tout le monde v5.1"));
+                .andExpect(content().string("Bonsoir tout le monde v6"));
     }
 }
