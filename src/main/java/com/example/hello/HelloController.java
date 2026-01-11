ec71474c3c34592c089bda9513469daea1a1ea45
safaa <128332698+Safaa1033@users.noreply.github.com>
2026-01-11 15:39:51 +0100

    Test webhook jenkins v6.2

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Bonsoir tout le monde v6.1";
+        return "Bonsoir tout le monde v6.2";
     }
 }
